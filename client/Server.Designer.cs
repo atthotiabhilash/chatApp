df1fbaade18f30a7f6d186ccbabb7c28681470b9
atthoti.abhilash <atthoti.abhilash@LP-PF3HPHDC>
2023-07-26 15:28:29 +0530

    Add project files.

---
+++ client/Server.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace server
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            outputBox = new TextBox();
+            clear = new Button();
+            SuspendLayout();
+            // 
+            // outputBox
+            // 
+            outputBox.Location = new Point(146, 24);
+            outputBox.Multiline = true;
+            outputBox.Name = "outputBox";
+            outputBox.Size = new Size(533, 272);
+            outputBox.TabIndex = 1;
+            outputBox.TextChanged += outputBox_TextChanged;
+            // 
+            // clear
+            // 
+            clear.Location = new Point(567, 302);
+            clear.Name = "clear";
+            clear.Size = new Size(112, 34);
+            clear.TabIndex = 2;
+            clear.Text = "Clear";
+            clear.UseVisualStyleBackColor = true;
+            clear.Click += clear_Click;
+            // 
+            // MessageForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(clear);
+            Controls.Add(outputBox);
+            Name = "MessageForm";
+            Text = "MessageForm";
+            Load += MessageForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label Message_Received;
+        private Label Message;
+        private TextBox outputBox;
+        private Button clear;
+    }
+}
---
+++ client/Server.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using serverdll;
+
+namespace server
+{
+    public partial class Server : Form
+    {
+        private TcpListener server;
+        private TcpClient client;
+        private Thread listenerThread;
+        private NetworkStream stream;
+        
+        public Server()
+        {
+            InitializeComponent();
+            client = new TcpClient();
+        }
+
+        private void MessageForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void StartListening()
+        {
+            Int32 portNumber = 8888;
+            string IpAddress = "127.0.0.1";
+            //"127.0.0.1"
+            IPAddress address = IPAddress.Parse(IpAddress);
+            server = new TcpListener(address, portNumber);
+            try
+            {
+                server.Start();
+
+                while (true)
+                {
+                    client = server.AcceptTcpClient();
+                    NetworkStream stream = client.GetStream();
+                    byte[] data = new byte[1024];
+                    int bytesRead = stream.Read(data, 0, data.Length);
+                    string msg = Encoding.ASCII.GetString(data, 0, bytesRead);
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        outputBox.Text += "Client: " + msg + "\r\n";
+                    });
+                    string message1 = "Server: Received the message";
+                    byte[] data1 = Encoding.ASCII.GetBytes(message1);
+                    stream.Write(data1, 0, data1.Length);
+                }
+            }
+            catch (Exception ex)
+            {  
+                outputBox.Text= ex.ToString();
+
+            }
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            listenerThread = new Thread(StartListening);
+            listenerThread.Start();
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            //listenerThread.Abort();
+        }
+        private void outputBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void clear_Click(object sender, EventArgs e)
+        {
+            outputBox.Text = string.Empty;
+        }
+    }
+}
